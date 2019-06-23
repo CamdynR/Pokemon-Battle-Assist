511f67f2a2161ee3e87126ebcf9f39fef103f248
CamdynR <rasque.camdyn@gmail.com>
2019-06-22 21:09:36 -0700

    Changed server.js to work with Heroku

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ const app = express();
 app.use(express.static('static_files'));
 
 // start the server at URL: http://localhost:3000/
-app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000/');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${ PORT }`);
 });
